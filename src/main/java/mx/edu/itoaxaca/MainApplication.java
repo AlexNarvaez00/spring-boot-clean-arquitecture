aca20283cfe12e4284dee9fcb52ef2987e120666
anaravez1 <anarvaez@mx.mbelatam.com>
2024-04-08 22:12:38 -0600

    feat: :tada: Init project

---
+++ src/main/java/mx/edu/itoaxaca/MainApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.itoaxaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
