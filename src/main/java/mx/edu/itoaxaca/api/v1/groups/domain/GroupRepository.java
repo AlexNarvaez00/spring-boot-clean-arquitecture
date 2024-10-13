002107d6bc6bf5893a309c5a7f110650878da618
anaravez1 <anarvaez@mx.mbelatam.com>
2024-10-13 12:35:44 -0600

    lost changes

--- src/main/java/mx/edu/itoaxaca/api/v1/groups/domain/GroupRepository.java
+++ src/main/java/mx/edu/itoaxaca/api/v1/groups/domain/GroupRepository.java
@@ -5,9 +5,12 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import mx.edu.itoaxaca.api.v1.shared.domain.criteria.Criteria;
+
 public interface GroupRepository {
     Group save(Group group);
     Page<Group> findAll(Pageable pageable);
     void deleteById(UUID id);
     Group findGroupById(UUID id);
+    Page<Group> findByCriteria(Criteria criteria, Pageable pageable);
 }
--- src/main/java/mx/edu/itoaxaca/api/v1/groups/infrastructure/persistences/PostgresGroupRespository.java
+++ src/main/java/mx/edu/itoaxaca/api/v1/groups/infrastructure/persistences/PostgresGroupRespository.java
@@ -3,6 +3,20 @@ package mx.edu.itoaxaca.api.v1.groups.infrastructure.persistences;
 import java.util.UUID;
 import mx.edu.itoaxaca.api.v1.groups.domain.Group;
 import mx.edu.itoaxaca.api.v1.groups.domain.GroupRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import mx.edu.itoaxaca.api.v1.shared.domain.criteria.Criteria;
+import mx.edu.itoaxaca.api.v1.shared.infrastructure.JPACriteriaConverter;
 
-public interface PostgresGroupRespository extends JpaRepository<Group, UUID>, GroupRepository {}
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PostgresGroupRespository
+    extends JpaRepository<Group, UUID>, JpaSpecificationExecutor<Group> ,GroupRepository {
+    default Page<Group> findByCriteria(Criteria criteria, Pageable pageable) {
+        JPACriteriaConverter<Group> converter = new JPACriteriaConverter<
+            Group
+        >();
+        return this.findAll(converter.convert(criteria), pageable);
+    }
+}
