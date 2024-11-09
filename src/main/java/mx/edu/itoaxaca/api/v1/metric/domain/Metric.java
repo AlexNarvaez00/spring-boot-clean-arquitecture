af2b1b8e3a2df28df3cc3b1c8e49a87c8735fcab
anaravez1 <anarvaez@mx.mbelatam.com>
2024-11-09 17:23:48 -0600

    Lost changes

---
+++ src/main/java/mx/edu/itoaxaca/api/v1/metric/domain/Metric.java
@@ -0,0 +1,5 @@
+package mx.edu.itoaxaca.api.v1.metric.domain;
+
+import java.util.UUID;
+
+public record Metric(UUID id, MetricName name) {}
---
+++ src/main/java/mx/edu/itoaxaca/api/v1/metric/domain/MetricName.java
@@ -0,0 +1,11 @@
+package mx.edu.itoaxaca.api.v1.metric.domain;
+
+import mx.edu.itoaxaca.api.v1.shared.domain.ValueObject.StringValueObject;
+
+public class MetricName extends StringValueObject{
+
+    public MetricName(String value) {
+        super(value);
+    }
+
+}
