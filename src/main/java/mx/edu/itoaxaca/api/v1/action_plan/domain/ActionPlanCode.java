4dafa8ab32c437eadcc67890ae85fb014c4e1029
anaravez1 <anarvaez@mx.mbelatam.com>
2024-12-31 23:49:03 -0600

    feat: lost changes

--- src/main/java/mx/edu/itoaxaca/api/v1/action_plan/domain/ActionPlanCode.java
+++ src/main/java/mx/edu/itoaxaca/api/v1/action_plan/domain/ActionPlanCode.java
@@ -2,7 +2,8 @@ package mx.edu.itoaxaca.api.v1.action_plan.domain;
 
 import mx.edu.itoaxaca.api.v1.shared.domain.ValueObject.StringValueObject;
 
-public class ActionPlanCode extends StringValueObject{
+public class ActionPlanCode extends StringValueObject {
+
     public ActionPlanCode(String value) {
         super(value);
     }
---
+++ src/main/java/mx/edu/itoaxaca/api/v1/shared/domain/Entity.java
@@ -0,0 +1,5 @@
+package mx.edu.itoaxaca.api.v1.shared.domain;
+
+public abstract class Entity <T> {
+    public abstract T toPrimitives();
+}
